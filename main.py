dba4bef2385d9895999f56053e0bdd546f7adbe3
Gustavo Botelho Vieira <gu-botelho@hotmail.com>
2024-08-26 22:38:47 -0300

    adicionando conexao com aws e ao bucket s3

--- main.py
+++ main.py
@@ -3,6 +3,23 @@ from typing import List
 import boto3
 from dotenv import load_dotenv
 
+# Carrega as variaveis de ambient do arquivo .env
+load_dotenv()
+
+#Configurações da AWS a partir do .env
+AWS_ACCESS_KEY_ID: str = os.getenv('AWS_ACESS_KEY_ID')
+AWS_SECRET_ACESS_KEY: str = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_REGION: str = os.getenv('AWS_REGION')
+BUCKET_NAME: str = os.getenv('BUCKET_NAME')
+
+# Classes de python
+s3_client = boto3.client(
+    's3',
+    aws_access_key_id=AWS_ACCESS_KEY_ID,
+    AWS_SECRET_ACESS_KEY=AWS_SECRET_ACESS_KEY,
+    region_name=AWS_REGION
+)
+
 ## LE OS ARQUIVOS
 
 ## JOGA OS ARQUIVOS PRO S3
